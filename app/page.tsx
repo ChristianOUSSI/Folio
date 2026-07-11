fecacf49ca90b90274460cec6b1b7d05f649ad8f
ChristianOUSSI <wassatherese@gmail.com>
2026-07-11 19:45:29 +0200

    Fix useEffect import in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Navbar from '../components/Navbar';
 import Launcher from '../components/Launcher';
 import { ErrorBoundary } from '../components/ErrorBoundary';
