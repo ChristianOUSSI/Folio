307f14909da75e470728b7f980ae0af4a1eccf7e
ChristianOUSSI <wassatherese@gmail.com>
2026-07-16 16:55:28 +0200

    Fix CSP: allow unsafe-inline for script-src to fix hydration errors

--- next.config.js
+++ next.config.js
@@ -35,7 +35,7 @@ const nextConfig = {
         },
         {
           key: 'Content-Security-Policy',
-          value: "default-src 'self'; script-src 'self' 'unsafe-eval' https://www.googletagmanager.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://gstatic.com; img-src 'self' data: https:; connect-src 'self'; frame-ancestors 'none';",
+          value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://www.googletagmanager.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://gstatic.com https://fonts.gstatic.com; img-src 'self' data: https:; connect-src 'self'; frame-ancestors 'none';",
         },
       ],
     },
